b97817caed686c8e262bccee67d0885e101c2c21
Emmanuel-Mmanda <luneya17@gmail.com>
2024-07-16 17:12:47 +0300

    m

--- mocare_project/settings.py
+++ mocare_project/settings.py
@@ -92,7 +92,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'mocare', 'static'),
+    os.path.join(BASE_DIR, 'mocare_project', 'static'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
